212dcf655689122b1b56067d6bd9c59456ac4781
mehedipro3 <mehediratul301910@gmail.com>
2025-09-26 15:52:56 +0600

    comments added

--- Index.js
+++ Index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ee44r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +28,8 @@ async function run() {
 
     const userCollection = client.db("appVenture").collection("users");
     const productsCollection = client.db("appVenture").collection("products");
+    const contactUsCollection = client.db("appVenture").collection("contactUs");
+    const commentsCollection = client.db("appVenture").collection("comments");
 
     //jwt related API
 
@@ -64,10 +66,30 @@ async function run() {
       res.send(result);
     });
 
-      //Products
+    //Products
 
     app.get("/products", async (req, res) => {
       const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // contactUs
+    app.post("/contactUs", async (req, res) => {
+      const user = req.body;
+      const result = await contactUsCollection.insertOne(user);
+      res.send(result);
+    });
+    // comments
+    app.post("/comments", async (req, res) => {
+      const user = req.body;
+      const result = await commentsCollection.insertOne(user);
       res.send(result);
     });
 
