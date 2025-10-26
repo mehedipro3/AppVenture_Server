f494497d86e850e9a7e393dbc6ed7be192669897
mehedipro3 <mehediratul37@gmail.com>
2025-10-26 23:37:57 +0600

    middlewares update

--- Index.js
+++ Index.js
@@ -41,20 +41,20 @@ async function run() {
 
     // middlewares
 
-    const verifyToken = (req, res, next) => {
-      console.log("Insider Verify Token", req.headers.authorization);
-      if (!req.headers.authorization) {
-        return res.status(401).send({ message: "unauthorized  access" });
-      }
-      const token = req.headers.authorization.split(" ")[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(401).send({ message: "unauthorized access" });
-        }
-        req.decoded = decoded;
-        next();
-      });
-    };
+    // const verifyToken = (req, res, next) => {
+    //   console.log("Insider Verify Token", req.headers.authorization);
+    //   if (!req.headers.authorization) {
+    //     return res.status(401).send({ message: "unauthorized  access" });
+    //   }
+    //   const token = req.headers.authorization.split(" ")[1];
+    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       return res.status(401).send({ message: "unauthorized access" });
+    //     }
+    //     req.decoded = decoded;
+    //     next();
+    //   });
+    // };
 
     // verifyAdmin
     const verifyAdmin = async (req, res, next) => {
