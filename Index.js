4057a79ceb7e159498801dc0058caf0bc3e04323
mehedipro3 <mehediratul37@gmail.com>
2025-11-18 11:52:32 +0600

    finish

--- Index.js
+++ Index.js
@@ -31,61 +31,68 @@ async function run() {
 
     //jwt related API
 
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
-      res.send({ token });
-    });
+    // app.post("/jwt", async (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1h",
+    //   });
+    //   res.send({ token });
+    // });
+
 
     // middlewares
 
-    const verifyToken = (req, res, next) => {
-      console.log("Insider Verify Token", req.headers.authorization);
-      if (!req.headers.authorization) {
-        return res.status(401).send({ message: "unauthorized  access" });
-      }
-      const token = req.headers.authorization.split(" ")[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(401).send({ message: "unauthorized access" });
-        }
-        req.decoded = decoded;
-        next();
-      });
-    };
+    // const verifyToken = (req, res, next) => {
+    //   const authHeader = req.headers.authorization;
+
+    //   console.log("Verify Token:", authHeader); // DEBUG
+
+    //   if (!authHeader) {
+    //     return res.status(401).send({ message: "unauthorized access" });
+    //   }
+
+    //   const token = authHeader.split(" ")[1];
+
+    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       return res.status(403).send({ message: "forbidden access" });
+    //     }
+    //     req.decoded = decoded;
+    //     next();
+    //   });
+    // };
 
     // verifyAdmin
-    const verifyAdmin = async (req, res, next) => {
-      const email = req.params.email;
-      const query = { email: email };
-      const user = await userCollection.findOne(query);
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.params.email;
+    //   const query = { email: email };
+    //   const user = await userCollection.findOne(query);
 
-      const isAdmin = user?.role === "admin";
-      if (!isAdmin) {
-        return res.status(403).send({ message: "Forbidden access" });
-      }
-      next();
-    };
+    //   const isAdmin = user?.role === "admin";
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: "Forbidden access" });
+    //   }
+    //   next();
+    // };
+
+    
 
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
 
-    app.get(`/users/admin/:email`, async (req, res) => {
+    app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
-      if (email != req.email) {
+
+      if (email !== req.decoded.email) {
         return res.status(403).send({ message: "forbidden access" });
       }
+
       const query = { email: email };
       const user = await userCollection.findOne(query);
-      let admin = false;
-      if (user) {
-        admin = user?.role === "admin";
-      }
-      res.send({ admin });
+
+      res.send({ admin: user?.role === "admin" });
     });
 
     app.get("/users/:email", async (req, res) => {
@@ -96,11 +103,12 @@ async function run() {
 
     app.patch("/users/:email", async (req, res) => {
       const email = req.params.email;
-      const updatedInfo = req.body;
+
       const result = await userCollection.updateOne(
         { email },
-        { $set: updatedInfo }
+        { $set: { role: "admin" } }
       );
+
       res.send(result);
     });
 
@@ -126,8 +134,6 @@ async function run() {
 
     app.patch(
       "/users/admin/:id",
-      verifyToken,
-      verifyAdmin,
       async (req, res) => {
         const id = req.params.id;
         const filter = { _id: new ObjectId(id) };
@@ -158,6 +164,11 @@ async function run() {
     app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.get("/reported-products", async (req, res) => {
+      const result = await reportedCollection.find().toArray();
       res.send(result);
     });
 
